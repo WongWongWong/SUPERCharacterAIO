dba595ef572062b52880ec74db9a129edaf4a798
Danny <381558466@qq.com>
2022-07-04 20:21:43 +0800

    123

---
+++ Assets/Test.cs
@@ -0,0 +1,56 @@
+﻿using SUPERCharacter;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    public int targetFPS = 30;
+    SUPERCharacterAIO _aio;
+    private void Awake()
+    {
+        _aio = GameObject.FindObjectOfType<SUPERCharacterAIO>();
+        Application.targetFrameRate = targetFPS;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F1))
+        {
+            if (Application.targetFrameRate < targetFPS)
+            {
+                Application.targetFrameRate = targetFPS;
+            }
+            else
+            {
+                Application.targetFrameRate = 10;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            _aio.PausePlayer(PauseModes.MakeKinematic);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            _aio.PausePlayer(PauseModes.FreezeInPlace);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            _aio.PausePlayer(PauseModes.BlockInputOnly);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            _aio.UnpausePlayer();
+        }
+    }
+}
